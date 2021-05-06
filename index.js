dbd166aa5a5ef5fef6d7084e3c703971b9d64705
justinctollison <justinctollison@gmail.com>
2021-05-06 15:06:53 -0700

    Why isn't it working?

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+function superbowlWin(record)
+{
+    function winningYear(element, index, record)
+    {
+        if(element.result === "W")
+        {
+            return element.result
+        }
+    }
+    const obj = record.find(winningYear)
+    return obj.year
+}
